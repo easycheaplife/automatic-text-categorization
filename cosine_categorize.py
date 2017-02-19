902f91e389e5330b5c9a022bf2ee796c8a601eab
sophia <lijin157@163.com>
2017-02-19 11:58:29 +0800

    cosine cayehorize batch

--- cosine_categorize.py
+++ cosine_categorize.py
@@ -6,25 +6,23 @@ from util_tool import *
 import numpy
 
 def categorization_files(path):
-	files = search_directory(path,'voc')
+	files = search_directory(path,'vec')
 	for input_name in files:
-		lines = open(input_name,"r")
-		for line in lines:
-			data = line.split()	
-			word = data[0]
-			word_no = data[1]
-			tf_idf = data[6]
-	return []
+		print input_name
+		categorization_file(input_name)
 
 def categorization_file(vec_file):
 	handle_froms = open(vec_file,'r')	
+	final_file = vec_file.replace('vec','final')
+	handle_final = open(final_file,'w')	
 	for from_line in handle_froms:
 		from_data = from_line.split()
 		handle_tos = open(vec_file,'r')
 		for to_line in handle_tos:
 			to_data = to_line.split()
 			cosine_value = compute_cosine_value(from_data,to_data)
-			print from_data[0] + "\t" + to_data[0] + "\t" + str(cosine_value) + "\n"
+			handle_final.write(from_data[0] + "\t" + to_data[0] + "\t" + str(cosine_value) + "\n")
+	handle_final.close()
 
 def compute_cosine_value(vec_a,vec_b):
 	# the first element is file name, skip it
@@ -32,6 +30,7 @@ def compute_cosine_value(vec_a,vec_b):
 	len_b = len(vec_b) - 1
 	vec_a = vec_a[1:len_a]
 	vec_b = vec_b[1:len_b]
+	# conver string to int
 	vec_a = [ int (vec_a) for vec_a in vec_a if vec_a ]
 	vec_b = [ int (vec_b) for vec_b in vec_b if vec_b ]
 	# conver array to vector, if not do this, TypeError: can't multiply sequence by non-int of type 'list'
@@ -40,8 +39,11 @@ def compute_cosine_value(vec_a,vec_b):
 	#	cos(a,b)=a*b/(|a|+|b|)
 	numerator = numpy.sum(vec_a*vec_b) 
 	denominator = float(numpy.sqrt(sum(numpy.square(vec_a))) * numpy.sqrt(sum(numpy.square(vec_b))))
-	theta = numerator / denominator
+	if 0 == denominator:
+		return 0
+	theta = round(numerator / denominator,4)
 	return theta
 
 categorization_file("./text/C00000810.vec")
+categorization_files("./text")
 
